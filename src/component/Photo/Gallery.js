38e4b1207ad5d7af148e00f5c86eb514bc906669
DIILEESHA <dileeeshanawarathna18@gmail.com>
2023-12-03 03:11:22 +0530

    create homepage view components

---
+++ src/component/Photo/Gallery.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./gallery.css";
+
+const Gallery = () => {
+  return (
+    <div className="gallery_container">
+      <div className="gallery_grid">
+        <div className="gallery_sub_grid">
+          <img
+            src="https://images.unsplash.com/photo-1503341455253-b2e723bb3dbb?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div>
+        <div className="gallery_sub_grid">
+          <img
+            src="https://images.unsplash.com/photo-1503341338985-c0477be52513?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div>
+
+        <div className="gallery_sub_grid jk">
+          <h3>men</h3>
+          <h2>The base collection - Ideal every day.</h2>
+          <button>shop now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -2,14 +2,17 @@ import React from "react";
 import Slid from "../slidr/Slid";
 import Homecard from "../category/homeCategory/Homecard";
 import Parallaxd from "../parallax/Parallaxd";
+import Gallery from "../Photo/Gallery";
+import Trendcard from "../category/homeCategory/Trrendcard";
 
 const Home = () => {
   return (
     <div>
       <Slid />
       <Homecard />
+      <Gallery />
       <Parallaxd />
-      <Homecard />
+      <Trendcard />
     </div>
   );
 };
